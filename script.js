da75a57c1fa33edcc3dca0c8801e7da8915dd2a1
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-03-30 13:40:47 -0300

    Version 0.6

---
+++ script.js
@@ -0,0 +1,71 @@
+const food =0;
+const drink = 0;
+const desert = 0;
+const frango = 14.90;
+const carne = 20.90;
+const trio = 19.90;
+const batata = 15.90;
+const lasanha = 19;
+const coca = 4.90;
+const guarana = 3.90;
+const fanta = 3.90;
+const coca2 = 6.90;
+const guarana2 = 5.90;
+const pudim = 7.90;
+const brigadeiro = 4;
+const bolo = 5.90;
+const sorvete = 5;
+const doce = 6.90;
+
+function foodselect1(food) {
+    food = 14.90;
+    return food;
+}
+function foodselect2(food) {
+    food = 22.90;
+}
+function foodselect3(trio) {
+    food = trio;
+}
+function foodselect4(batata) {
+    food = batata;
+}
+function foodselect5(lasanha) {
+    food = lasanha;
+}
+function drinkselect1(coca) {
+    drink = coca;
+}
+function drinkselect2(guarana) {
+    drink = guarana;
+}
+
+function drinkselect3(fanta) {
+    drink = fanta;
+}
+
+function drinkselect4(coca2) {
+    drink = coca2;
+}
+
+function drinkselect5(guarana2) {
+    drink = guarana2;
+}
+function desertselect1(pudim){
+    desert=pudim;
+}
+function desertselect2(brigadeiro){
+    desert=brigadeiro
+}
+function desertselect3(bolo){
+    desert=bolo;
+}
+function desertselect4(sorvete){
+    desert=sorvete;
+}
+function desertselect5(doce){
+    desert=doce;
+}
+
+const total=food+drink+desert;
+
