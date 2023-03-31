ee3967d440446f383aed68d643919781d696ac19
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-03-31 00:09:56 -0300

    Versão 0.9 com Tela de finalizar pedido

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let drink;
-let drinkname;
-let desert;
-let desertname;
 const frango = 14.90;
 const carne = 20.90;
 const trio = 19.90;
@@ -19,9 +15,10 @@ const sorvete = 5;
 const doce = 6.90;
 
 function foodselect(food) {
-    const botaoanterior = document.querySelector('.selected');
+    const botaoanterior = document.querySelector('.foods .selected');
+    console.log(botaoanterior);
     if (botaoanterior !== null) {
-        botaoanterior.classList.remove('.selected');
+        botaoanterior.classList.remove('selected');
     }
 
     const botao = document.querySelector(food);
@@ -30,29 +27,54 @@ function foodselect(food) {
 
 
 function drinkselect(drink) {
-    const botaoanterior = document.querySelector('.selected');
+    const botaoanterior = document.querySelector('.drinks .selected');
+    console.log(botaoanterior);
     if (botaoanterior !== null) {
-        botaoanterior.classList.remove('.selected')
-
-        const botao = document.querySelector(drink);
-        botao.classList.add('selected');
-    }}
-
-
-    function desertselect(desert) {
-        const botaoanterior = document.querySelector('.selected');
-        if (botaoanterior !== null) {
-            botaoanterior.classList.remove('.selected')
-
-            const botao = document.querySelector(desert);
-            botao.classList.add('selected');
-        }}
+        botaoanterior.classList.remove('selected');
+    }
+    const botao = document.querySelector(drink);
+    botao.classList.add('selected');
+}
 
 
-        function final() {
-            const total = food + drink + desert;
-            alert(total.toFixed(2))
-        }
+function desertselect(desert) {
+    const botaoanterior = document.querySelector('.deserts .selected');
+    console.log(botaoanterior);
+    if (botaoanterior !== null) {
+        botaoanterior.classList.remove('selected');
+    }
+    const botao = document.querySelector(desert);
+    botao.classList.add('selected');
+}
+
+
+function final() {
+    const food = document.querySelector('.foods .selected .preco h2 ');
+    const drink = document.querySelector('.drinks .selected .preco h2');
+    const desert = document.querySelector('.deserts .selected .preco h2');
+    const foodname = document.querySelector('.foods .selected .foodname h1');
+    const drinkname = document.querySelector('.drinks .selected .foodname h1');
+    const desertname = document.querySelector('.deserts .selected .foodname h1');
+    const total = Number(food.innerHTML) + Number(drink.innerHTML) + Number(desert.innerHTML);
+    console.log(total.toFixed(2));
+    console.log(foodname)
+    document.querySelector('.confirm').style.display = "flex";
+    let name1 = document.querySelector('.confirm .box .op .fo');
+    let name2 = document.querySelector('.confirm .box .op .dr');
+    let name3 = document.querySelector('.confirm .box .op .de');
+    name1.innerHTML= foodname.innerHTML;
+    name2.innerHTML= drinkname.innerHTML;
+    name3.innerHTML=desertname.innerHTML;
+    let p1 = document.querySelector('.confirm .box .op .fop');
+    let p2 = document.querySelector('.confirm .box .op .drp');
+    let p3 = document.querySelector('.confirm .box .op .dep');
+    p1.innerHTML= food.innerHTML;
+    p2.innerHTML= drink.innerHTML;
+    p3.innerHTML=desert.innerHTML;
+    let tot=document.querySelector('.confirm .box .op .total');
+    tot.innerHTML=total.toFixed(2);
+}
+
 
 
 
